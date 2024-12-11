e782b9341d52f13883f31a7a6e769b349928b9a3
Ali Hanafiah <hanafiahali267@gmail.com>
2024-12-11 21:18:15 +0700

    update db config

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: true },
+    // ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
